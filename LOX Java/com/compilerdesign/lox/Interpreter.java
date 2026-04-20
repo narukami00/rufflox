188a401094c9c4f58f8cf1661526c5a8674bba82
Rafsan Riasat <riasat1011@gmail.com>
2026-04-20 11:15:57 +0600

    chapter 9: if else and logical operators (and, or) implemented

--- LOX Java/com/compilerdesign/lox/Interpreter.java
+++ LOX Java/com/compilerdesign/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package com.compilerdesign.lox;
 
+import java.io.ObjectStreamException;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
@@ -43,6 +44,19 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitLiteralExpr(Expr.Literal expr){
         return expr.value;
+    }
+
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr){
+        Object left=evaluate(expr.left);
+
+        if(expr.operator.type==TokenType.OR){
+            if(isTruthy(left)) return left;
+        }else{
+            if(!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
     }
 
     @Override
@@ -118,6 +132,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt){
         evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt){
+        if(isTruthy(evaluate(stmt.condition))){
+            execute(stmt.thenBranch);
+        }else if(stmt.elseBranch != null){
+            execute(stmt.elseBranch);
+        }
         return null;
     }
 
